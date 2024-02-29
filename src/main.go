858d7418c1fc2ae2c1a4a3c8ff594bc3868c1792
FernandoCelmer <email@fernandocelmer.com>
2024-02-28 23:32:17 -0300

    ⚙️ FEATURE: Test Main

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func main() {
+	app := NewApp(
+		Title("API"),
+		Description("Minimalist API Test"),
+		Version("0.1.0"),
+	)
+
+	app.Get("/item", itemHandler)
+	app.Xpto("/xpto", itemHandler, ContentType("application/json"))
+
+	app.Run(
+		Host("127.0.0.5"),
+		Port(8080),
+	)
+}
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func itemHandler(w http.ResponseWriter, r *http.Request) {
+	data := Response{Message: "Item"}
+	response, _ := json.Marshal(data)
+	w.Write(response)
+}
